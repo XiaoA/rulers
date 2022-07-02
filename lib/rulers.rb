0b3db9300f0d82ad5fca60859705b4d47a9585b4
Andrew Buckingham <abuckingham99@gmail.com>
2022-07-01 22:37:44 -0700

    Initial commit

---
+++ lib/rulers.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative "rulers/version"
+
+module Rulers
+  class Error < StandardError; end
+
+  class Application
+    def call(env)
+      [200, {'Content-Type' => 'text/html'},
+       ["Hello from Ruby on Rulers!"]]
+    end
+  end
+end
---
+++ test/test_rulers.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestRulers < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Rulers::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
