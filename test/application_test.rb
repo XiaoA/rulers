35c1cf1e0fccf1f05a1ff3c270d68e57c9198510
Andrew Buckingham <abuckingham99@gmail.com>
2022-07-02 16:03:08 -0700

    Add tests

---
+++ test/application_test.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require_relative "test_helper"
+
+class TestApp < Rulers::Application  
+end
+
+class RulersAppTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    TestApp.new
+  end
+
+  def test_request
+    get "/"
+
+
+    assert last_response.ok?
+    
+    body = last_response.body
+    assert body["Hello"]
+  end  
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,5 +2,6 @@
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "rulers"
+require "rack/test"
 
 require "minitest/autorun"
