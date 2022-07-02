49b5805b8d20d9a66314b2c2719b3583fe137478
Andrew Buckingham <abuckingham99@gmail.com>
2022-07-02 08:51:27 -0700

    Add debugging script

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -7,6 +7,7 @@ module Rulers
 
   class Application
     def call(env)
+      `echo debug > debug.txt`;
       [200, {'Content-Type' => 'text/html'},
        ["Hello from Ruby on Rulers!"]]
     end
